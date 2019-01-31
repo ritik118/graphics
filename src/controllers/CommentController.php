fd41b076fe264ea1a94f43489e71c0e694bc65db
ritik118 <ritikkumar6081@gmail.com>
2019-01-31 16:29:29 +0530

    name change

--- src/controllers/CommentController.php
+++ src/controllers/CommentController.php
@@ -8,6 +8,16 @@ use ritik\dynamicgraphs\models\Comment;
 use ritik\dynamicgraphs\models\Questioning;
 class CommentController extends Controller
 {
+
+	public function storenew(){
+		for($i=1;$i<35;$i++){
+			$data=[
+			'ques_id' => $i,
+			'comment' => "Ritik"
+		];
+		Questioning::create($data);
+		}
+	}
     
 	public function store(Request $request){
 		$comment=$request->comments;
