ffe1ede687e4c406305a78ec8b020e7470fbdc0c
ritik118 <ritikkumar6081@gmail.com>
2019-01-31 13:36:08 +0530

    change

--- src/DynamicGraphsServiceProvider.php
+++ src/DynamicGraphsServiceProvider.php
@@ -27,7 +27,7 @@ class DynamicGraphsServiceProvider extends ServiceProvider
             __DIR__.'/storage' => base_path('storage'),
         ]);
           $this->publishes([
-        __DIR__.'/assets' => base_path('public');
+        __DIR__.'/assets' => base_path('public/js');
     }
 
     /**
